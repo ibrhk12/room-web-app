064b17329fc292869364b22f82c54f773b5d4dc7
ibrhk12 <ibrahimkassem1092@gmail.com>
2024-01-31 20:44:07 +0200

    room with rest controller, and service

---
+++ src/main/java/com/ik/sbet/landon/roomwebapp/service/RoomService.java
@@ -0,0 +1,18 @@
+package com.ik.sbet.landon.roomwebapp.service;
+
+import com.ik.sbet.landon.roomwebapp.models.Room;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RoomService {
+    private static final List<Room> rooms = new ArrayList<>();
+    static {
+        for(int i = 1;i <= 10; i++){
+            rooms.add(new Room(i, "Room "+ i, "R " + i, "Q"));
+        }
+    }
+    public List<Room> getRooms(){ return rooms;}
+}
